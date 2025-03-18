fb9c6ff1e28e6f71bf60eb8a2ce02582f6291712
uihyunglee <uihyunglee.tech@gmail.com>
2025-03-19 02:14:18 +0900

    TEST: Add example of config and orderbook publication

---
+++ acds/config_example.py
@@ -0,0 +1,18 @@
+EXCHANGE_CONFIG = {
+    "okx": {
+        "ws_url": "wss://ws.okx.com:8443/ws/v5/public",
+        "api_key": "",
+        "secret_key": "",
+        "exchange": "OKX",
+        "orderbook_port": 57102,
+        "position_port" : 57202,
+    },
+    "bybit": {
+        "ws_url": "wss://stream.bybit.com/v5/public/spot",
+        "api_key": "",
+        "secret_key": "",
+        "exchange": "BYBIT",
+        "orderbook_port": 57103,
+        "position_port" : 57203,
+    }
+}
---
+++ example/orderbook_pub.py
@@ -0,0 +1,40 @@
+import time
+from threading import Thread
+
+from acds.exchange.okx import OkxOrderBookPublisher
+# from acds.exchange.bybit import BybitOrderBookPublisher
+
+from acds.config_example import EXCHANGE_CONFIG
+
+TIME_SLEEP = 1000  # DEFAULT: float('inf')
+
+
+if __name__ == '__main__':
+    pub_ob_okx = OkxOrderBookPublisher(
+        ws_url=EXCHANGE_CONFIG["okx"]["ws_url"],
+        symbols=["BTC-USDT", "ETH-USDT"],
+        api_key='',
+        secret_key='',
+        exchange=EXCHANGE_CONFIG["okx"]["exchange"],
+        zmq_port=EXCHANGE_CONFIG["okx"]["orderbook_port"],
+        save_mode=True
+    )
+    # bybit_orderbook_publisher = BybitOrderBookPublisher(
+    #     ws_url=EXCHANGE_CONFIG["bybit"]["ws_url"],
+    #     symbols=["BTCUSDT", "ETHUSDT"],
+    #     api_key='',
+    #     secret_key='',
+    #     exchange=EXCHANGE_CONFIG["bybit"]["exchange"],
+    #     zmq_port=EXCHANGE_CONFIG["bybit"]["orderbook_port"]
+    # )
+    
+    threads = []
+    pubs = [v for k, v in globals().items() if k.startswith('pub')]
+    for pub in pubs: threads.append(Thread(target=pub.start))
+    
+    for t in threads: t.start()
+        
+    time.sleep(TIME_SLEEP)
+
+    for pub in pubs: pub.end()
+    for t in threads: t.join()
